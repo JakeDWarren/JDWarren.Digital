8465dbd494799b86a7b850aef771d31acca6456d
(s) Jake Warren <jdwarren@pm7-0272.uopnet.plymouth.ac.uk>
2018-03-16 18:14:00 +0000

    Second Commit -Navbar created -Background to black

--- JS/sketch.js
+++ JS/sketch.js
@@ -4,7 +4,7 @@ function parallax() {
 var parallax1 = document.getElementById('parallax1');
 var parallax2 = document.getElementById('parallax2');
 var parallax3 = document.getElementById('parallax3');
-parallax1.style.top = (window.pageYOffset/3)+'px';
-parallax2.style.top = (window.pageYOffset/5)+'px';
+parallax1.style.top = (window.pageYOffset/2)+'px';
+parallax2.style.top = (window.pageYOffset/3)+'px';
 parallax2.style.top = -(window.pageYOffset/0)+'px';
 }
