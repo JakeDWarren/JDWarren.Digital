4a3f89c9f6dfbb2024d29d1d8bbd02a5e80eee85
Jake Warren <jakewarren@pc-138-172.customer.dairy.vic.lan>
2018-03-23 00:01:43 +0000

    Added Javascript to control background color according to time

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,10 +1,32 @@
 window.addEventListener("scroll", parallax);
 
 function parallax() {
-var parallax1 = document.getElementById('parallax1');
-var parallax2 = document.getElementById('parallax2');
-var parallax3 = document.getElementById('parallax3');
-parallax1.style.top = (window.pageYOffset/2)+'px';
-parallax2.style.top = (window.pageYOffset/3)+'px';
-parallax2.style.top = -(window.pageYOffset/0)+'px';
+  var parallax1 = document.getElementById('parallax1');
+  var parallax2 = document.getElementById('parallax2');
+  var parallax3 = document.getElementById('parallax3');
+  parallax1.style.top = (window.pageYOffset/2)+'px';
+  parallax2.style.top = (window.pageYOffset/3)+'px';
+  parallax2.style.top = -(window.pageYOffset/0)+'px';
 }
+
+window.onload = function() {
+  currentYear();
+};
+
+function currentYear(){
+  const date = new Date();
+  const autoDate = document.querySelector('#autoDate');
+  autoDate.textContent = date.getFullYear();
+};
+
+//Function to set background to black when time is between 8pm and 7.59am
+function currentYear(){
+  var d = new Date();
+  var n = d.getHours();
+
+  if(n>=20 || n<=7){
+    var element = document.getElementById("body");
+    element.classList.add("timeD");
+    console.log(n);
+  }
+};
